c12ea9aecce1d27c7dd5298bd587f61355c30853
Willy Fox <official.blackvikingpro@gmail.com>
2019-02-21 16:46:50 -0500

    initial commit

---
+++ app/tunhidden.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Welcome to Tunhidden!
+This project is a collaboration effort by Willy Fox (@BlackVikingPro) and r0otz-ee (@r0otshell).
+Tunhidden is a tool coded in Python designed for discovering Tor Hidden Services that server owners may be running incognito on a given VPS, website, etc.
+"""
+
+import os, sys, time, socket
+
+
+# do something
